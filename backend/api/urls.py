9a3f40d7dd79a92190576afc70e991e5c4bb6389
Junkyu Park <junqwoopark@gmail.com>
2023-03-15 19:41:43 +0900

    fix: urls.py path 수정

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 from .views import login
 from .views import get_my_info
-from .views import get_user_info
-from .views import get_log
-from .views import user
+from .views import get_locations, get_locations_stats
+from .views import get_bears_user_info
 
 urlpatterns = [
     path('login/', login),
     path('user/', get_my_info),
-    path('user/bear', get_bears_user_info)
-    path('user/<str:login>/', get_user_info),
-    path('log/', get_log),
+    path('user/bear', get_bears_user_info),
+    path('locations/', get_locations),
+    path('locations_stats/', get_locations_stats),
 ]
