d62e0aee2bf58b76716ede14c04616809212ae9c
Junkyu Park <junqwoopark@gmail.com>
2023-03-17 03:11:20 +0900

    chore: Database 수정

--- backend/bearapi/settings.py
+++ backend/bearapi/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'bearapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bear',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
